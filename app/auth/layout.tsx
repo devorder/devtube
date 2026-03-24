fdf08bc7781680fbed11026e50c559fd8b56ceb3
Satyam Sharma <satyam.sharmadev@gmail.com>
2026-03-24 07:36:33 +0530

    Layout restructuring

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-
+import type { Metadata } from "next";
+import "../globals.css";
+export const metadata: Metadata = {
+  title: "DevTube - Sign In",
+  description:
+    "Sign in to your DevTube account to access your personalized dashboard, manage your videos, and connect with the community. Enter your email and password to get started.",
+};
 const AuthLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
-  return <div className="w-full h-full">{children}</div>;
+  return (
+    <html lang="en">
+      <body className={``}>
+        <div className="w-full h-full">{children}</div>;
+      </body>
+    </html>
+  );
 };
 export default AuthLayout;
